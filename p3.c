aab7eafc388c617608ae82b02bdfdcaa8bbe3481
MBpoornima <87684721+MBpoornima@users.noreply.github.com>
2022-12-26 16:04:56 +0530

    Add files via upload

---
+++ p3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ int main()
+ {
+ int k; // variable declaration
+ for(k=0;k<=125;k++) // for loop from 0-255
+ {
+ printf("\nThe ascii value of %c is %d", k,k);
+ }
+ return 0;
+}
---
+++ p4.c
@@ -0,0 +1,8 @@
+//integer literal//
+ #include <stdio.h>
+ int main()
+ {
+ const int a=23; // constant integer literal
+ printf("Integer literal : %d", a);
+ return 0;
+}
